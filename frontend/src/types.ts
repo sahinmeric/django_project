c519cdad2ef477e12d609498637d78e7f4e0009b
Sahin Meric <sahin.meric@medisprout.com>
2024-07-09 17:39:15 -0500

    added types

---
+++ frontend/src/types.ts
@@ -0,0 +1,52 @@
+export interface Empresa {
+  empresa: number;
+  nombre: string;
+  estado: string;
+}
+
+export interface Cliente {
+  id_cliente: number;
+  empresa: number;
+  cliente: number;
+  tipo: string;
+  nombre: string;
+  telefono: string;
+  correo: string;
+  direccion: string;
+  paginaweb: string;
+  fecha_creacion: string;
+  estado: string;
+}
+
+export interface Producto {
+  id_producto: number;
+  empresa: number;
+  producto: string;
+  nombre: string;
+  tipo: string;
+  fotoproducto: string;
+  fecha_creacion: string;
+  estado: string;
+}
+
+export interface MaeFactura {
+  id_factura: number;
+  empresa: number;
+  cliente: number;
+  numero: number;
+  fecha_factura: string;
+  id_cliente: number;
+  observaciones: string;
+  total: number;
+  fecha_auditoria: string;
+}
+
+export interface DetFactura {
+  id_factura: number;
+  empresa: number;
+  consecutivo: number;
+  id_producto: number;
+  cantidad: number;
+  precio: number;
+  sub_total: number;
+}
