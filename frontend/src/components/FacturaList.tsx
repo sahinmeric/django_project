d9fff6980c7c891ce98153ceaeca4c70e6224d63
Sahin Meric <sahin.meric@medisprout.com>
2024-07-10 23:54:54 -0500

    factura list updated

--- frontend/src/components/FacturaList.tsx
+++ frontend/src/components/FacturaList.tsx
@@ -1,36 +1,58 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, Stack } from "@mui/material";
 import api from "../services/api";
 import { MaeFactura } from "../types";
+import Loader from "./Loader";
 
 const FacturaList: React.FC = () => {
   const [facturas, setFacturas] = useState<MaeFactura[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     api
-      .get("facturas/")
+      .get<MaeFactura[]>("facturas/")
       .then((response) => {
         setFacturas(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.error("There was an error fetching the facturas!", error);
+        setLoading(false);
       });
   }, []);
 
+  if (loading) return <Loader />;
+
   return (
-    <div>
-      <h1>Facturas List</h1>
-      <ul>
+    <Stack alignItems={"center"} sx={{ mt: 6 }} spacing={2}>
+      <Stack alignSelf={"center"} maxWidth="md" sx={{ m: 2 }}>
+        <Typography variant="h6">FACTURAS</Typography>
+      </Stack>
+      <Stack alignContent={"center"} spacing={2}>
         {facturas.map((factura) => (
-          <li key={factura.id_factura}>
-            <Link to={`/factura/${factura.id_factura}`}>
-              {factura.numero} - {factura.fecha_factura} - {factura.total}
-            </Link>
-          </li>
+          <Card key={factura.id_factura} sx={{ mb: 2, width: 300 }}>
+            <CardContent>
+              <Typography variant="h6" component="h3">
+                Factura #{factura.numero}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                <strong>Fecha:</strong>{" "}
+                {new Date(factura.fecha_factura).toLocaleDateString()}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                <strong>Empresa:</strong> {factura.empresa}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                <strong>Observaciones:</strong> {factura.observaciones}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                <strong>Total:</strong> ${factura.total}
+              </Typography>
+            </CardContent>
+          </Card>
         ))}
-      </ul>
-      <Link to="/new-factura">Create New Factura</Link>
-    </div>
+      </Stack>
+    </Stack>
   );
 };
 
